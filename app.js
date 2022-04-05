95ea8b60854750ccb408b00bbf94d8bb94b864f5
Dipta437 <53457948+Dipta437@users.noreply.github.com>
2022-04-06 01:31:56 +0600

    added api and fetch call

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputTxt = document.querySelector("#input-text");
 var outputTxt = document.querySelector("#output-text");
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text= " + text
+}
+
 
 function clickHandler(){
-    outputTxt.innerText = inputTxt.value + " awesome"
+    // Taking Input
+    var textInput = inputTxt.value;
+
+    // calling server for processing
+
+    fetch(getTranslationURL(textInput))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputTxt.innerText = translatedText;
+    })
 }
 
 btnTranslate.addEventListener("click", clickHandler);
